fd18220309021194909a64f0bd350f601435b461
Hemant Desale <137862595+hdesale2141@users.noreply.github.com>
2026-04-05 11:39:19 +0530

    Update CORS configuration for production frontend

--- src/main/java/com/example/BookStore_Production/config/CorsConfig.java
+++ src/main/java/com/example/BookStore_Production/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://your-frontend-url.vercel.app")
+                        .allowedOrigins("http://localhost:5173", "https://bookstore-production-frontend-react.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
