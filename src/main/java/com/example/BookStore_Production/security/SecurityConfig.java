0bf5937bc07e23735c5f17038a329a9d35d98ac9
Hemant Desale <137862595+hdesale2141@users.noreply.github.com>
2026-04-05 12:16:40 +0530

    Add CORS configuration to SecurityConfig

--- src/main/java/com/example/BookStore_Production/security/SecurityConfig.java
+++ src/main/java/com/example/BookStore_Production/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -27,6 +32,21 @@ public class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter){
         this.jwtFilter = jwtFilter;
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(List.of("*")); // TEMP FIX
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
@@ -35,6 +55,7 @@ public class SecurityConfig {
                         .requestMatchers("/user/register", "/user/login", "/user/refresh", "/book/").permitAll()
                         .anyRequest().authenticated()
                 )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
